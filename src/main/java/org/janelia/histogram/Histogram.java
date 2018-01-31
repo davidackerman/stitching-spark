869bab6a5fcd674de2e081c56fcba4a0c75e2903
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-01-31 17:07:59 -0500

    make histogram class compatible with n5 spark downsampler

--- src/main/java/org/janelia/histogram/Histogram.java
+++ src/main/java/org/janelia/histogram/Histogram.java
@@ -2,12 +2,17 @@ package org.janelia.histogram;
 
 import java.io.Serializable;
 
-public class Histogram implements Serializable
+import net.imglib2.type.Type;
+import net.imglib2.type.operators.Add;
+import net.imglib2.type.operators.MulFloatingPoint;
+import net.imglib2.util.Util;
+
+public class Histogram implements Type< Histogram >, Add< Histogram >, MulFloatingPoint, Serializable
 {
 	private static final long serialVersionUID = -3130834243396947444L;
 
-	private final double[] histogram;
-	private final double minValue, maxValue, binWidth;
+	private double[] histogram;
+	private double minValue, maxValue, binWidth;
 	private double quantityTotal, quantityLessThanMin, quantityGreaterThanMax;
 
 	public Histogram( final double minValue, final double maxValue, final int bins )
@@ -17,12 +22,6 @@ public class Histogram implements Serializable
 		this.minValue = minValue;
 		this.maxValue = maxValue;
 		this.binWidth = ( maxValue - minValue ) / bins;
-	}
-
-	protected Histogram()
-	{
-		histogram = null;
-		minValue = maxValue = binWidth = 0;
 	}
 
 	public int getNumBins() { return histogram.length; }
@@ -36,6 +35,11 @@ public class Histogram implements Serializable
 	public double get( final int bin )
 	{
 		return histogram[ bin ];
+	}
+
+	public double[] getHistogram()
+	{
+		return histogram.clone();
 	}
 
 	public double getBinValue( final int bin )
@@ -68,6 +72,7 @@ public class Histogram implements Serializable
 		quantityTotal += quantity;
 	}
 
+	@Override
 	public void add( final Histogram other )
 	{
 		for ( int bin = 0; bin < getNumBins(); ++bin )
@@ -80,11 +85,71 @@ public class Histogram implements Serializable
 
 	public void average( final long numHistograms )
 	{
-		for ( int bin = 0; bin < getNumBins(); ++bin )
-			histogram[ bin ] /= numHistograms;
+		mul( 1. / numHistograms );
+	}
 
-		quantityTotal /= numHistograms;
-		quantityLessThanMin /= numHistograms;
-		quantityGreaterThanMax /= numHistograms;
+	@Override
+	public void mul( final float ratio )
+	{
+		mul( ( double ) ratio );
+	}
+
+	@Override
+	public void mul( final double ratio )
+	{
+		for ( int bin = 0; bin < getNumBins(); ++bin )
+			histogram[ bin ] *= ratio;
+
+		quantityTotal *= ratio;
+		quantityLessThanMin *= ratio;
+		quantityGreaterThanMax *= ratio;
+	}
+
+	@Override
+	public boolean valueEquals( final Histogram other )
+	{
+		if ( histogram.length != other.histogram.length )
+			return false;
+
+		if ( !Util.isApproxEqual( minValue, other.minValue, 1e-9 ) || !Util.isApproxEqual( maxValue, other.maxValue, 1e-9 ) )
+			return false;
+
+		if ( !Util.isApproxEqual( quantityLessThanMin, other.quantityLessThanMin, 1e-9 ) || !Util.isApproxEqual( quantityGreaterThanMax, other.quantityGreaterThanMax, 1e-9 ) )
+			return false;
+
+		if ( !Util.isApproxEqual( quantityTotal, other.quantityTotal, 1e-9 ) )
+			return false;
+
+		for ( int bin = 0; bin < histogram.length; ++bin )
+			if ( !Util.isApproxEqual( histogram[ bin ], other.histogram[ bin ], 1e-9 ) )
+				return false;
+
+		return true;
+	}
+
+	@Override
+	public Histogram createVariable()
+	{
+		return new Histogram( minValue, maxValue, histogram.length );
+	}
+
+	@Override
+	public Histogram copy()
+	{
+		final Histogram copy = new Histogram( minValue, maxValue, histogram.length );
+		copy.set( this );
+		return copy;
+	}
+
+	@Override
+	public void set( final Histogram other )
+	{
+		histogram = other.histogram.clone();
+		minValue = other.minValue;
+		maxValue = other.maxValue;
+		binWidth = other.binWidth;
+		quantityTotal = other.quantityTotal;
+		quantityLessThanMin = other.quantityLessThanMin;
+		quantityGreaterThanMax = other.quantityGreaterThanMax;
 	}
 }
